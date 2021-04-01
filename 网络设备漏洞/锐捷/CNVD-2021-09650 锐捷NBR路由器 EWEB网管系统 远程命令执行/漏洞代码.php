d9152a9cd192ce31c5f646fb8a44f2fea379459a
Firebasky <63966847+Firebasky@users.noreply.github.com>
2021-04-01 18:21:54 +0800

    Update 漏洞代码.php

--- 网络设备漏洞/锐捷/CNVD-2021-09650 锐捷NBR路由器 EWEB网管系统 远程命令执行/漏洞代码.php
+++ 网络设备漏洞/锐捷/CNVD-2021-09650 锐捷NBR路由器 EWEB网管系统 远程命令执行/漏洞代码.php
@@ -4,8 +4,8 @@ $usermac = @$_POST['mac'];​    
 if (!$userip || !$usermac) {  
 exit;   
 }    /* 判断该用户是否已经放行 */    
-$cmd = '/sbin/app_auth_hook.elf -f ' . $userip;    
-$res = exec($cmd, $out, $status);    /* 如果已经上线成功 */    
+$cmd = '/sbin/app_auth_hook.elf -f ' . $userip;    #直接进行命令拼接
+$res = exec($cmd, $out, $status);    /* 如果已经上线成功 */    
 if (strstr($out[0], "status:1")) {        
 echo 'true'; 
 }
